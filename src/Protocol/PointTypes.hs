c569811e87491992e349d3b38d82ab70d7b961d4
Scott Murphy <scottmurphy09@gmail.com>
2013-10-30 14:20:20 -0500

    Fixed up better prototypes for the newtypes

--- src/Protocol/PointTypes.hs
+++ src/Protocol/PointTypes.hs
@@ -1,30 +1,91 @@
+{-# LANGUAGE TupleSections, OverloadedStrings, QuasiQuotes, TemplateHaskell, TypeFamilies, RecordWildCards,
+             DeriveGeneric ,MultiParamTypeClasses ,FlexibleInstances  #-}
+
 module Protocol.ROC.PointTypes where
 
 -- newtype PointType1
+data PointType1 = PointType1 
+{
+ pointType1PointTag             :: PointType1PointTag          
+,pointType1Filter               :: PointType1Filter                   
+,pointType1Status               :: PointType1Status                    
+,pointType1BitfieldHigh         :: PointType1BitfieldHigh                   
+,pointType1BitfieldLow          :: PointType1BitfieldLow                   
+,pointType1AccumulatedValues    :: PointType1AccumulatedValues                   
+,pointType1OnCounter            :: PointType1OnCounter                   
+,pointType1OffCounter           :: PointType1OffCounter                   
+,pointType1PulseWidth0          :: PointType1PulseWidth0                   
+,pointType1PulseWidth100        :: PointType1PulseWidth100                   
+,pointType1MaxTimePulse         :: PointType1MaxTimePulse                   
+,pointType1Units                :: PointType1Units                   
+,pointType1ScanPeriod           :: PointType1ScanPeriod                   
+,pointType1LowReading           :: PointType1LowReading                   
+,pointType1HighReading          :: PointType1HighReading                   
+,pointType1LowAlarm             :: PointType1LowAlarm                   
+,pointType1HighAlarm            :: PointType1HighAlarm                   
+,pointType1LowLowAlarm          :: PointType1LowLowAlarm                   
+,pointType1HiHiAlarm            :: PointType1HiHiAlarm                   
+,pointType1RateAlarm            :: PointType1RateAlarm                   
+,pointType1AlarmDeadband        :: PointType1AlarmDeadband                   
+,pointType1EUValue              :: PointType1EUValue                   
+,pointType1TDICount             :: PointType1TDICount                    
+} deriving (Read,Eq, Show, Generic)                       
+                       
+newtype PointType1PointTag           = PointType1PointTag         
+  deriving (Read,Eq, Show, Generic)                                 
+newtype PointType1Filter             = PointType1Filter             
+  deriving (Read,Eq, Show, Generic)                                 
+newtype PointType1Status             = PointType1Status             
+  deriving (Read,Eq, Show, Generic)                                 
+newtype PointType1BitfieldHigh       = PointType1BitfieldHigh       
+  deriving (Read,Eq, Show, Generic)                                 
+newtype PointType1BitfieldLow        = PointType1BitfieldLow        
+  deriving (Read,Eq, Show, Generic)                                 
+newtype PointType1AccumulatedValues  = PointType1AccumulatedValues  
+  deriving (Read,Eq, Show, Generic)                                 
+newtype PointType1OnCounter          = PointType1OnCounter          
+  deriving (Read,Eq, Show, Generic)                                 
+newtype PointType1OffCounter         = PointType1OffCounter         
+  deriving (Read,Eq, Show, Generic)                                 
+newtype PointType1PulseWidth0        = PointType1PulseWidth0        
+  deriving (Read,Eq, Show, Generic)                                 
+newtype PointType1PulseWidth100      = PointType1PulseWidth100      
+  deriving (Read,Eq, Show, Generic)                                 
+newtype PointType1MaxTimePulse       = PointType1MaxTimePulse       
+  deriving (Read,Eq, Show, Generic)                                 
+newtype PointType1Units              = PointType1Units              
+  deriving (Read,Eq, Show, Generic)                                 
+newtype PointType1ScanPeriod         = PointType1ScanPeriod         
+  deriving (Read,Eq, Show, Generic)                                 
+newtype PointType1LowReading         = PointType1LowReading         
+  deriving (Read,Eq, Show, Generic)                                 
+newtype PointType1HighReading        = PointType1HighReading        
+  deriving (Read,Eq, Show, Generic)                                 
+newtype PointType1LowAlarm           = PointType1LowAlarm           
+  deriving (Read,Eq, Show, Generic)                                 
+newtype PointType1HighAlarm          = PointType1HighAlarm          
+  deriving (Read,Eq, Show, Generic)                                 
+newtype PointType1LowLowAlarm        = PointType1LowLowAlarm        
+  deriving (Read,Eq, Show, Generic)                                 
+newtype PointType1HiHiAlarm          = PointType1HiHiAlarm          
+  deriving (Read,Eq, Show, Generic)                                 
+newtype PointType1RateAlarm          = PointType1RateAlarm          
+  deriving (Read,Eq, Show, Generic)                                 
+newtype PointType1AlarmDeadband      = PointType1AlarmDeadband      
+  deriving (Read,Eq, Show, Generic)                                 
+newtype PointType1EUValue            = PointType1EUValue            
+  deriving (Read,Eq, Show, Generic)                                 
+newtype PointType1TDICount           = PointType1TDICount           
+  deriving (Read,Eq, Show, Generic)                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
 
--- data PointType1PointTag
--- data PointType1Filter
--- data PointType1Status 
--- data PointType1BitfieldHigh
--- data PointType1BitfieldLow
--- data PointType1AccumulatedValues
--- data PointType1OnCounter
--- data PointType1OffCounter
--- data PointType1PulseWidth0
--- data PointType1PulseWidth100
--- data PointType1MaxTimePulse
--- data PointType1Units
--- data PointType1ScanPeriod
--- data PointType1LowReading
--- data PointType1HighReading
--- data PointType1LowAlarm
--- data PointType1HighAlarm
--- data PointType1LowLowAlarm
--- data PointType1HiHiAlarm
--- data PointType1RateAlarm
--- data PointType1AlarmDeadband
--- data PointType1EUValue
--- data PointType1TDICount 
 
 
 
@@ -48,3 +109,41 @@ module Protocol.ROC.PointTypes where
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
